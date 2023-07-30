9af2f2a057784d5c9a5da81b9d868db956b7718c
sushil026 <ranasushil026@gmail.com>
2023-07-30 18:38:17 +0530

    Updated cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './Component/Card'
 import Form from './Component/Form'
 import './App.css'
 export default function App() {
-  
+  const [values, setValues] = useState(['Jane Appleseed', '0000 0000 0000 0000', '00', '00', '000']);
+  function handleFill(data) {
+    setValues(data);
+  }
   return (
     <>
-    <div id='screen'></div>
-    <div>
-    <Form/>
-    <Card/>
-    </div>
+      <div id='screen'></div>
+      <div>
+        <Form onSubmit={handleFill} />
+        <Card cardHolder={values[0]} cardNumber={values[1]} expMonth={values[2]} expYear={values[3]} cvc={values[4]} />
+      </div>
     </>
-    )
+  )
 }
--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,35 +1,50 @@
 import React from "react";
 import "./Card.css";
-function Card() {
+import PropTypes from 'prop-types'
+function Card({ cardHolder, cardNumber, expMonth, expYear, cvc }) {
   return (
     <>
       <div id="parent">
         {/* front of CARD */}
         <div id="front" className="cards">
-         <div id="logos">
+          <div id="logos">
             <div id="d1"></div>
             <div id="d2"></div>
-         </div>
-         <div id="details">
-               <h1>0000 0000 0000 0000</h1>
+          </div>
+          <div id="details">
+            <h1>{cardNumber}</h1>
             <div id="data">
-                  <h3>JANE APPLESEED</h3>
-                  <h3>00 / 00</h3>
+              <h3>{cardHolder.toUpperCase()}</h3>
+              <h3> {expMonth} / {expYear}</h3>
             </div>
-         </div>
+          </div>
         </div>
         {/* back of CARD */}
         <div id="rear" className="cards">
-         <div id="label"></div>
-         <div id="cvc-block">
-            <h3>000</h3>
-         </div>
-         <div id="third">
+          <div id="label"></div>
+          <div id="cvc-block">
+            <h3>{cvc}</h3>
+          </div>
+          <div id="third">
             <h3>------- ------ ----------------- ---------- ---------- ----------- -------- -------- -- ------- --</h3>
-         </div>
+          </div>
         </div>
       </div>
     </>
   );
 }
+Card.propTypes = {
+  cardHolder: PropTypes.string,
+  cardNumber: PropTypes.string,
+  expMonth: PropTypes.string,
+  expYear: PropTypes.string,
+  cvc: PropTypes.string,
+};
+Card.defalutProps = {
+  cardHolder: 'Jane Appleseed',
+  cardNumber: '0000 0000 0000 0000',
+  expMonth: '00',
+  expYear: '00',
+  cvc: '000'
+};
 export default Card;
