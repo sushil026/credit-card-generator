11c9881df703766b51252cd0508fad3883f47120
sushil026 <ranasushil026@gmail.com>
2023-08-08 13:06:30 +0530

    added success message component

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -19,26 +19,31 @@ export default function Form(props) {
   }
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (name.length <= 0 || number.length !== 16 || month.length < 1 || year.length < 2 || cvc.length !== 3 || Number.isInteger(cvc) || Number.isInteger(year) || Number.isInteger(month) || !between(parseInt(month, 0, 13)) || parseInt(year) < 23) {
-      setError(true);
-    }
-    if (error) {
+    if (!(name.length <= 0 || number.length !== 16 || month.length < 1 || year.length < 2 || cvc.length !== 3 || Number.isInteger(cvc) || Number.isInteger(year) || Number.isInteger(month) || !between(parseInt(month, 0, 13)) || parseInt(year) < 23)) {
       let data = [name, number, month, year, cvc];
       props.onSubmit(data);
     }
+    else{
+      setError(true);
+    }
   }
+  // if (!error) {
+  //   // console.log(error);
+  //   let data = [name, number, month, year, cvc];
+  //   props.onSubmit(data);
+  // }
   return (
     <form>
       <div id="name">
         <h2>CARDHOLDER NAME</h2>
-        <input className="input1" type="text" placeholder="e.g. Jane Appleseed" onChange={e => setName(e.target.value)} />
+        <input className="input1" type="text" placeholder="e.g. Jane Appleseed" onBlur={e => setName(e.target.value)} />
         <br />
         {error && name.length <= 0 ?
           <label>Cardholder name is required</label> : ""}
       </div>
       <div id="cardNumber">
         <h2>CARD NUMBER</h2>
-        <input className="input1" type="text" placeholder="e.g. 1234 5678 9000 0000" onChange={e => setNumber(e.target.value)} />
+        <input className="input1" type="text" placeholder="e.g. 1234 5678 9000 0000" onBlur={e => setNumber(e.target.value)} />
         <br />
         {error && (number.length !== 16) ?
           <label>Card number must be 16 digit number</label> : ""}
@@ -46,15 +51,15 @@ export default function Form(props) {
       <div id="second">
         <div id="exp">
           <h2>EXP. DATE</h2>
-          <input type="text" className="input2" id="month" placeholder="MM" onChange={e => setMonth(e.target.value)} />
-          <input type="text" className="input2" id="year" placeholder="YY" onChange={e => setYear(e.target.value)} />
+          <input type="text" className="input2" id="month" placeholder="MM" onBlur={e => setMonth(e.target.value)} />
+          <input type="text" className="input2" id="year" placeholder="YY" onBlur={e => setYear(e.target.value)} />
           <br />
           {error && (Number.isInteger(year) || Number.isInteger(month) || month.length < 1 || year.length < 2 || !between(parseInt(month, 0, 13)) || parseInt(year) < 23) ?
             <label>Input valid Date</label> : ""}
         </div>
         <div id="cvc">
           <h2>CVC</h2>
-          <input type="text" id="input3" placeholder="e.g. 123" onChange={e => setCVC(e.target.value)} />
+          <input type="text" id="input3" placeholder="e.g. 123" onBlur={e => setCVC(e.target.value)} />
           <br />
           {error && (cvc.length !== 3 || Number.isInteger(cvc)) ?
             <label>CVC must be 3 digit number</label> : ""}
